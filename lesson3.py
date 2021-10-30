7332ae703c31e96bf0dc42e274e5addf6ea65936
xelaxela13 <xelaxela13@gmail.com>
2021-10-30 12:45:47 +0300

    lesson3

---
+++ lesson3.py
@@ -0,0 +1,15 @@
+input_string = input('Введите что нибудь: ')
+initial_string = "Это строка в которую {} новую строку".format(input_string)
+
+print(initial_string)
+
+initial_string = initial_string.replace(input_string, 'замена в строке')
+
+print(initial_string)
+
+print(len(initial_string))
+
+if initial_string.find('строка'):
+    print('Да')
+else:
+    print('Нет')
