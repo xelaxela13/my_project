ee4cd5c4e7fa1355a53dfbbae18d47a6cb6ff194
xelaxela13 <xelaxela13@gmail.com>
2021-11-14 20:37:55 +0200

    lesson6

--- lesson6.py
+++ lesson6.py
@@ -215,20 +215,13 @@ dict_2 = {k: v for k, v in
           zip([random.choice(string.ascii_lowercase) for _ in range(10)],
               [random.randint(1, 100) for _ in range(10)])}
 
-dict_3 = {}
+dict_3 = dict_1 | dict_2
 
-if len(dict_1) > len(dict_2):
-    first = dict_1
-    second = dict_2
-else:
-    first = dict_2
-    second = dict_1
-
-print(first)
-print(second)
-for k, v in first.items():
-    if k in second.keys():
-        v = second[k] if v < first[k] else v
+print(dict_1)
+print(dict_2)
+for k, v in dict_3.items():
+    if k in dict_1.keys():
+        v = dict_1[k] if v < dict_3[k] else v
     dict_3[k] = v
 
 print(dict_3)
